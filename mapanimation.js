e647735cea748378b9013fdb8d62bcdd4781c41b
Erick Suárez <esuarez_97@hotmail.com>
2022-05-19 18:02:24 -0500

    Add files via upload

---
+++ mapanimation.js
@@ -0,0 +1,44 @@
+const busStops = [
+    [-71.093729, 42.359244],
+    [-71.094915, 42.360175],
+    [-71.0958, 42.360698],
+    [-71.099558, 42.362953],
+    [-71.103476, 42.365248],
+    [-71.106067, 42.366806],
+    [-71.108717, 42.368355],
+    [-71.110799, 42.369192],
+    [-71.113095, 42.370218],
+    [-71.115476, 42.372085],
+    [-71.117585, 42.373016],
+    [-71.118625, 42.374863],
+  ];
+  
+  mapboxgl.accessToken = 'pk.eyJ1IjoiYnVyZG9teCIsImEiOiJjbDMzM3E0cDcwaHBiM2pxajVvMzZqM2MzIn0.P3GFUypLMGGZ3uHe--9F0g';
+  
+  let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/dark-v10',
+    center: [-71.104081, 42.365554],
+    zoom: 14,
+  });
+  
+  const otto = document.createElement('div');
+  otto.className = 'marker';
+
+  var marker = new mapboxgl.Marker(otto)
+    .setLngLat([-71.092761, 42.357575])
+    .addTo(map);
+  
+  let counter = 0;
+  function move() {
+    setTimeout(() =>{
+        if (counter >= busStops.length) return;
+        marker.setLngLat(busStops[counter]);
+        counter++;
+        move();
+      }, 300);
+  }  
+  function restart() {
+    marker.setLngLat([-71.092761, 42.357575]);
+    counter = 0;
+  }
